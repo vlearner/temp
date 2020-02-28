d1eb35cecd7a77fe4925d9d9beb45d89976d4328
Vishal <vishalbarot@ymail.com>
2020-02-28 03:39:19 -0600

    update

---
+++ mainScript.js
@@ -0,0 +1,7 @@
+$(document).ready(function() {
+    $('.selectTask').select2();
+});
+
+
+src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js";
+src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js";
